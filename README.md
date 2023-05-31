2aa79b35064cebbda66c0f98fc6bfe9cad1f154d
youyeul lee <yuyeul301@gmail.com>
2023-05-31 16:13:40 +0900

    hello world 출력 추가
    hello world 출력 구문 생성

--- README.md
+++ README.md
@@ -5,5 +5,6 @@
 ```java
 public static void main(String[] args){
     System.out.println("hell world");
+    System.out.println("hello world");
         }
 ```
--- src/main/java/com/ohgiraffers/Application.java
+++ src/main/java/com/ohgiraffers/Application.java
@@ -3,5 +3,6 @@ package com.ohgiraffers;
 public class Application {
     public static void main(String[] args) {
         System.out.println("hell world");
+        System.out.println("hello world");
     }
 }
